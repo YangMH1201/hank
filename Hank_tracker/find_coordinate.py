02e806476002ec88df71e96a5d56f6142b408584
YangMH1201 <135639931+YangMH1201@users.noreply.github.com>
2024-03-20 01:18:21 +0800

    Update find_coordinate.py

--- Hank_tracker/find_coordinate.py
+++ Hank_tracker/find_coordinate.py
@@ -89,6 +89,7 @@ async def start_mission(uavs, drone_lat_long, uwb_info):
         # UWB distance
         # await function.distance_data(relative_distances)
         relative_distances.append(get_uwb_dist(uwb_info))
+        print(f"distance={relative_distances[-1]}")
 
         if isinstance(target_position, np.ndarray):
             target_position = target_position
