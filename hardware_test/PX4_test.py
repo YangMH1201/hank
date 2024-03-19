1982ede494503212001d4e72deffa5c8cd1909fa
YangMH1201 <135639931+YangMH1201@users.noreply.github.com>
2024-03-19 15:28:09 +0800

    Add files via upload

---
+++ hardware_test/PX4_test.py
@@ -0,0 +1,26 @@
+from pymavlink import mavutil
+import time
+
+def connect_pixhawk(port="/dev/ttyUSB_PX4", baudrate=921600):
+    # 建立與Pixhawk的連接
+    master = mavutil.mavlink_connection(port, baud=baudrate)
+    print(f"連接到Pixhawk在 {port} 以 {baudrate} 波特率")
+
+    # 等待心跳以確認連接
+    print("等待心跳...")
+    master.wait_heartbeat()
+    print("心跳接收，Pixhawk連接正常！")
+
+    # 嘗試讀取一些基本信息
+    try:
+        while True:
+            # 獲取消息
+            msg = master.recv_match(blocking=True)
+            if msg:
+                print(f"接收到消息: {msg}")
+                break  # 獲取到第一條消息後退出循環
+    except KeyboardInterrupt:
+        print("程序被用戶中斷")
+
+if __name__ == "__main__":
+    connect_pixhawk()
---
+++ hardware_test/uwb_test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import serial
+import threading
+import time
+
+# COM_PORT = "COM6"
+COM_PORT = '/dev/ttyUSB_UWB'
+BAUD_RATES = 115200
+ser = serial.Serial(COM_PORT, BAUD_RATES)
+
+class UwbModule:
+    def __init__(self):
+
+        def serialthread():
+            while self.serthread_alive:
+                try:
+                    while self.serial.in_waiting:
+                        recv = self.serial.readline().decode()
+                        if 'device' not in recv and 'added' not in recv:
+                            #print("recv", recv)
+                            self.uwb_data = recv
+                        else:
+                            print("IN ELSE")
+                            print(recv)
+                            self.uwb_data = None
+                except Exception as err:
+                    print(f"arduino UWB error: {err}.......{self.uwb_data}")
+                    self.uwb_data = None
+
+        self.serial = ser
+        self.serthread_alive = True
+        self.serthread = threading.Thread(target=serialthread)
+        self.serthread.start()
+        self.uwb_data = None
+
+    def get_module_data(self):
+        # self.uwb_data = None
+        # self.uwb_data = 'dis'
+        return self.uwb_data
+
+    def close(self):
+        self.serthread_alive = False
+        self.serial.close()
+
+
+uwb = UwbModule()
+while True:
+    print(f"{uwb.get_module_data()}")
+    time.sleep(0.1)
