2bad6e750996eec1932ab4b99a460ad03048e598
YangMH1201 <135639931+YangMH1201@users.noreply.github.com>
2024-07-04 21:27:07 +0800

    Update offboard_setup.py

--- Hank_tracker/offboard_setup.py
+++ Hank_tracker/offboard_setup.py
@@ -112,7 +112,7 @@ async def local_position(drone, coordinate):
     async for position in drone.telemetry.position_velocity_ned():
         local_x = position.position.east_m
         local_y = position.position.north_m
-        local_z = position.relative_altitude_m  # 转换为相对高度
+        local_z = - position.position.down_m  # 转换为相对高度
         await asyncio.sleep(0.01)
         coordinate.append([local_x, local_y, local_z])
         break
