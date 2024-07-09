30a43f09dcafb3e8081ce61a2b08551539d5f6a3
YangMH1201 <135639931+YangMH1201@users.noreply.github.com>
2024-07-09 09:27:49 +0800

    Create main_test.py

---
+++ Hank_target/main_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import asyncio
+from mavsdk import System
+import offboard_setup
+import time
+
+
+async def main():
+    uwb_info = UwbModule()
+
+    # initialize paramters
+    target_height = 3.0
+    approach_distance = 2.5
+    velocity_North = 0.1
+    velocity_East = 0.0
+    velocity_down = 0.0
+
+    number = 0
+    drone_lat_long = list()
+
+    # Connect to the Simulation
+    uav_target = System()
+    uavs = [uav_target]
+
+    """ Start Algo initailization """
+    await offboard_setup.start_drones(uavs, drone_lat_long, target_height)
+    print("Start Algo")
+    asyncio.ensure_future(offboard_setup.set_drone_velocity(
+        uavs[0], velocity_North, velocity_East, velocity_down))
+
+    # Stop the mission, and land the drone
+    await offboard_setup.set_drone_velocity(uavs[0], 0.0, 0.0, 0.0)
+    await offboard_setup.stop_offboard_mode(uavs)
+
+    print("landing the target drone.")
+    await offboard_setup.drones_land(uavs)
+
+    # Wait for the tasks to finish
+    while True:
+        await asyncio.sleep(1)
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
